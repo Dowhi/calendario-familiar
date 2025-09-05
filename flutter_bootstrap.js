6da258169af53eac0de36b13fe74a0d8a39aac32
Dowhi <Dowhi@users.noreply.github.com>
2025-09-05 22:48:28 +0000

    deploy: 9557d33a2ec5a8af6abf46cbbc2c50682e701bf8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "316421427"
+    serviceWorkerVersion: "2550520913"
   }
 });
